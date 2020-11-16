a8885dd2892862c5ded364ed9df436a5241fbf96
Lucas <lucasabreusilva.98@gmail.com>
2020-11-16 07:19:59 -0300

    Inserido novos testes para fixação de conteúdo

--- libbaihanupython/tests/test_spam/test_enviador_de_email.py
+++ libbaihanupython/tests/test_spam/test_enviador_de_email.py
@@ -1,16 +1,39 @@
-from libbaihanupython.spam.enviador_de_email import Enviador
+import pytest
+
+from libbaihanupython.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-def test_remetente():
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['foo@bar.com.br', 'lucasabreusilva.98@gmail.com']
+)
+def test_remetente(remetente):
     enviador = Enviador()
     resultado = enviador.enviar(
-        'lucasabreusilva.98@gmail.com',
+        remetente,
         'lucaseduardoabreusilva@gmail.com',
         'Cursos Python Pro',
         'Turma Jessica Ferrari'
     )
-    assert 'lucasabreusilva.98@gmail.com' in resultado
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'lucasabreusilva.98']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+            remetente,
+            'lucaseduardoabreusilva@gmail.com',
+            'Cursos Python Pro',
+            'Turma Jessica Ferrari'
+        )
+        assert remetente in resultado
