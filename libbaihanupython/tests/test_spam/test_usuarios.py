92942be44350dbbae2e6f266ea52eac2b4ac7021
Lucas <lucasabreusilva.98@gmail.com>
2020-11-26 15:47:10 -0300

    Criado arquivo conftest

--- libbaihanupython/tests/test_spam/test_usuarios.py
+++ libbaihanupython/tests/test_spam/test_usuarios.py
@@ -1,25 +1,14 @@
-from libbaihanupython.spam.db import Conexao
 from libbaihanupython.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Lucas')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuario(sessao):
     usuarios = [Usuario(nome='Lucas'), Usuario(nome='Renzo')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
